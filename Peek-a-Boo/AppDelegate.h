59e989d9d1bc8628395198c5102f38c067a6f9a3
drw86312 <david.warner4@gmail.com>
2014-06-05 12:52:12 -0500

    Initial Commit

---
+++ Peek-a-Boo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Peek-a-Boo
+//
+//  Created by David Warner on 6/5/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Peek-a-Boo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Peek-a-Boo
+//
+//  Created by David Warner on 6/5/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
